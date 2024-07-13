e0c9d015d9ff930b365c816a276bed1c54af18af
Cynthia-mueni <nthenyamuenicynthia@gmail.com>
2024-07-13 22:44:35 +0300

    added files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import Cast from './Podcast';
 
 function App() {
-
-}
+    return(
+<div>
+   <Cast /> 
+</div>
+)
+};
 
 export default App;
---
+++ src/Podcast/index.js
@@ -0,0 +1,33 @@
+import "./index.css";
+
+const Cast = () => {
+  return (
+    <>
+      <div className="podcast">
+        <div className="pod">
+          <img src="/images/48353.jpg" />
+        </div>
+
+        <div className="text">
+          <h2>Welcome to Our Podcast</h2>
+          <div>
+            Hello, We are Techchix, and welcome to the ‘The Women In Tech’
+            podcast,<br></br>
+            where we cover all things robotic in K12 education! Today we have
+            the young women ,<br></br>
+            who are software Developers, and semi-professional Battle Bot
+            makers. We’re going to talk about the<br></br>
+            latest news of women in tech and how some of them got started to
+            become who they are now.<br></br>
+            I’m looking forward to an interesting fun interview with tech women.
+          </div>
+          <h3>Coming Soon</h3>
+          <button>Subscribe</button>
+          <p>follow us...</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Cast;
