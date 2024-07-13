f8e04c1a390d1a37491b0a066663165157142be3
Maureen-Njango <maureennjangombira@gmail.com>
2024-07-13 22:18:55 +0300

    born a crime blog

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import './App.css';
+
+import Blogs from './Blogs';
+
 
 function App() {
-
+  return (
+    <div className="App">
+      <Blogs/>
+      
+     
+    </div>
+  );
 }
 
 export default App;
